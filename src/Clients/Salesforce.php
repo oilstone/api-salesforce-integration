dfc8b01aae4d6bc6ded39f780479655151c4d3a8
Brendan Bullen <66941+bullenb@users.noreply.github.com>
2025-06-19 11:33:01 +0100

    Refactor Salesforce client
    Signed-off-by: Brendan Bullen <66941+bullenb@users.noreply.github.com>

--- src/Clients/Salesforce.php
+++ src/Clients/Salesforce.php
@@ -42,9 +42,14 @@ class Salesforce
         ]));
     }
 
-    protected function request(string $method, string $url, array $options): array
+    protected function request(string $method, string $url, array $options = []): array
     {
-        $response = $this->httpClient->request($method, $url, $options);
+        $response = $this->httpClient->request($method, $url, array_merge([
+            'headers' => [
+                'Authorization' => 'Bearer '.$this->accessToken,
+                'Accept' => 'application/json',
+            ],
+        ], $options));
 
         $body = (string) $response->getBody();
         $data = json_decode($body, true);
@@ -60,41 +65,17 @@ class Salesforce
 
     public function query(string $soql): array
     {
-        $data = $this->request('GET', $this->instanceUrl.'/services/data/'.$this->instanceVersion.'/query', [
-            'headers' => [
-                'Authorization' => 'Bearer '.$this->accessToken,
-                'Accept' => 'application/json',
-            ],
-            'query' => ['q' => $soql],
-        ]);
-
-        return $data['records'] ?? [];
+        return $this->request('GET', $this->instanceUrl.'/services/data/'.$this->instanceVersion.'/query', ['query' => ['q' => $soql]])['records'] ?? [];
     }
 
     public function describe(string $object): array
     {
-        $url = $this->instanceUrl.'/services/data/'.$this->instanceVersion.'/sobjects/'.trim($object, '/').'/describe';
-
-        $data = $this->request('GET', $url, [
-            'headers' => [
-                'Authorization' => 'Bearer '.$this->accessToken,
-                'Accept' => 'application/json',
-            ],
-        ]);
-
-        return $data;
+        return $this->request('GET', $this->instanceUrl.'/services/data/'.$this->instanceVersion.'/sobjects/'.trim($object, '/').'/describe');
     }
 
     public function picklistValues(string $object, string $recordTypeId, string $field): array
     {
-        $url = $this->instanceUrl.'/services/data/'.$this->instanceVersion.'/ui-api/object-info/'.trim($object, '/').'/picklist-values/'.trim($recordTypeId, '/').'/'.trim($field, '/');
-
-        $data = $this->request('GET', $url, [
-            'headers' => [
-                'Authorization' => 'Bearer '.$this->accessToken,
-                'Accept' => 'application/json',
-            ],
-        ]);
+        $data = $this->request('GET', $this->instanceUrl.'/services/data/'.$this->instanceVersion.'/ui-api/object-info/'.trim($object, '/').'/picklist-values/'.trim($recordTypeId, '/').'/'.trim($field, '/'));
 
         return array_values(array_map(
             fn ($v) => html_entity_decode($v['value'], ENT_QUOTES | ENT_HTML5),
@@ -104,41 +85,20 @@ class Salesforce
 
     public function create(string $object, array $payload): array
     {
-        $url = $this->instanceUrl.'/services/data/'.$this->instanceVersion.'/sobjects/'.trim($object, '/');
-
-        return $this->request('POST', $url, [
-            'headers' => [
-                'Authorization' => 'Bearer '.$this->accessToken,
-                'Content-Type' => 'application/json',
-                'Accept' => 'application/json',
-            ],
+        return $this->request('POST', $this->instanceUrl.'/services/data/'.$this->instanceVersion.'/sobjects/'.trim($object, '/'), [
             'json' => $payload,
         ]);
     }
 
     public function update(string $object, string $id, array $payload): array
     {
-        $url = $this->instanceUrl.'/services/data/'.$this->instanceVersion.'/sobjects/'.trim($object, '/').'/'.trim($id, '/');
-
-        return $this->request('PATCH', $url, [
-            'headers' => [
-                'Authorization' => 'Bearer '.$this->accessToken,
-                'Content-Type' => 'application/json',
-                'Accept' => 'application/json',
-            ],
+        return $this->request('PATCH', $this->instanceUrl.'/services/data/'.$this->instanceVersion.'/sobjects/'.trim($object, '/').'/'.trim($id, '/'), [
             'json' => $payload,
         ]);
     }
 
     public function delete(string $object, string $id): array
     {
-        $url = $this->instanceUrl.'/services/data/'.$this->instanceVersion.'/sobjects/'.trim($object, '/').'/'.trim($id, '/');
-
-        return $this->request('DELETE', $url, [
-            'headers' => [
-                'Authorization' => 'Bearer '.$this->accessToken,
-                'Accept' => 'application/json',
-            ],
-        ]);
+        return $this->request('DELETE', $this->instanceUrl.'/services/data/'.$this->instanceVersion.'/sobjects/'.trim($object, '/').'/'.trim($id, '/'));
     }
 }
