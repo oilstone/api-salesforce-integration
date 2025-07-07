b910e250e5cfa91c7e9ed2b9c2ec0682c1dcc9ed
Brendan Bullen <66941+bullenb@users.noreply.github.com>
2025-07-07 13:07:56 +0100

    Query cache handler
    Signed-off-by: Brendan Bullen <66941+bullenb@users.noreply.github.com>

--- src/Integrations/Laravel/ServiceProvider.php
+++ src/Integrations/Laravel/ServiceProvider.php
@@ -18,7 +18,7 @@ class ServiceProvider extends BaseServiceProvider
         $config = config('salesforce');
 
         $this->app->singleton(QueryCacheHandler::class, function ($app) use ($config) {
-            $cache = $app->make('cache.psr16');
+            $cache = $app->make('cache.store');
 
             return new QueryCacheHandler($cache, $config['query_cache_default_ttl']);
         });
