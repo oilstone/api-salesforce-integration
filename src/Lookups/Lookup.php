4197f8ec6aceb9635747bad26ae6c54122ad2fae
Brendan Bullen <66941+bullenb@users.noreply.github.com>
2025-08-04 14:24:56 +0100

    Refactor lookups
    Signed-off-by: Brendan Bullen <66941+bullenb@users.noreply.github.com>

--- src/Lookups/Lookup.php
+++ src/Lookups/Lookup.php
@@ -6,26 +6,5 @@ use Oilstone\ApiSalesforceIntegration\Clients\Salesforce;
 
 abstract class Lookup
 {
-    /**
-     * The Salesforce object name.
-     */
-    abstract public static function object(): string;
-
-    /**
-     * The field on the Salesforce object containing the picklist.
-     */
-    abstract public static function field(): string;
-
-    /**
-     * The record type id for the Salesforce object.
-     */
-    abstract public static function recordTypeId(): string;
-
-    /**
-     * Retrieve all picklist values for the lookup.
-     */
-    public static function fetch(Salesforce $client): array
-    {
-        return $client->picklistValues(static::object(), static::recordTypeId(), static::field());
-    }
+    abstract public static function fetch(Salesforce $client): array;
 }
